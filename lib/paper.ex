38d843035b1c6c936052e81153c78680f7b5d8ee
Yatender <ok@yimac.local>
2019-01-22 09:54:55 +0530

    Model migrated to paper - Repo is handled by server functions so it is moved to Paper module.

--- lib/paper.ex
+++ lib/paper.ex
@@ -6,4 +6,20 @@ defmodule Paper do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  def model do
+    quote do
+      use Ecto.Schema
+
+      import Ecto
+      import Ecto.Changeset
+      import Ecto.Query
+      alias Paper.Repo, as: Repo
+    end
+  end
+
+  defmacro __using__(which) when is_atom(which) do
+    apply(__MODULE__, which, [])
+  end
+
 end
