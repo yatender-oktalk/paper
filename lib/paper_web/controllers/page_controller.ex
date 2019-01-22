44da96853bb0cfff0139fc477caad270bbb9aa73
Yatender <ok@yimac.local>
2019-01-22 11:33:58 +0530

    added view layer in the controller

--- lib/paper_web/controllers/page_controller.ex
+++ lib/paper_web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule PaperWeb.PageController do
   use PaperWeb, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    {:ok, contents} = Paper.Content.Model.get_content(%{limit: 10, offset: 0})
+    render conn, "index.html", contents: contents
   end
 end
